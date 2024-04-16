754f8768e417dd4cdceb8518d340d485945ae174
Ppesto <s4004934@student.rmit.edu.au>
2024-04-16 14:38:54 +1000

    jave

---
+++ week5/script.js
@@ -0,0 +1,12 @@
+// console.log("my apolocheese🧀");
+// console.log("forev");
+// let city = prompt ("what is this city called");
+// console.log("this city is", city);
+
+let assignment1 = "2";
+let assignment2 = "23";
+let assignment3 = "34";
+
+let total = parseInt(assignment1) + parseInt(assignment2) + parseInt(assignment3);
+console.log ("my total score is", total );
+ 
