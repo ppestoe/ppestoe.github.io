18d4411cd7474d2bc64de90d63cb675cb739fca8
ppestoe <130018086+ppestoe@users.noreply.github.com>
2024-06-18 03:17:07 +1000

    s

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,30 +1,34 @@
-// let draggedElement = null;
+let draggedElement = null;
 
-// const colorBlob = document.querySelector(".blob");
-// const resultBlob = document.querySelector("#result");
+const colorBlob = document.querySelector(".blob");
+const resultBlob = document.querySelector(".result");
+const blobImg = document.querySelector("#blob");
 
-// colorBlob.addEventListener("dragstart", startDrag);
+colorBlob.addEventListener("dragstart", startDrag);
 
-// function startDrag() {
-//   draggedElement = colorBlob;
-// }
+function startDrag() {
+  draggedElement = colorBlob;
+}
 
-// resultBlob.addEventListener("dragover", endDrag);
+resultBlob.addEventListener("dragover", endDrag);
 
-// function endDrag(event) {
-//   event.preventDefault();
-// }
+function endDrag(event) {
+  event.preventDefault();
+}
 
-// resultBlob.addEventListener("drop", handleDrop);
+resultBlob.addEventListener("drop", handleDrop);
 
-// function handleDrop() {
-//   if (draggedElement) {
-//     const color = window
-//       .getComputedStyle(draggedElement)
-//       .getPropertyValue("background-color");
-//     // const color = draggedElement.style.backgroundColor;
-//     resultBlob.style.backgroundColor = color;
-//     resultBlob.textContent = "Dropped!";
-//     draggedElement = null;
-//   }
-// }
+function handleDrop() {
+  if (draggedElement) {
+    const color = window
+      .getComputedStyle(draggedElement)
+      .getPropertyValue("background-color");
+      resultBlob.classList.toggle("show");
+    resultBlob.style.backgroundColor = color;
+    draggedElement = null;
+    blobImg.src = "https://img.icons8.com/ios-filled/BC7C68/500/splash.png";
+  }
+  else {
+    blobImg.src = "https://img.icons8.com/ios/BC7C68/500/splash.png";
+  }
+}
