64eae471c9d6a0b873a437fc498dd2a61730858f
ppestoe <130018086+ppestoe@users.noreply.github.com>
2024-06-18 20:46:36 +1000

    sn

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,6 +1,5 @@
 // this is so that the other functions can refer to the dragged element
 let draggedElement = null;
-let droppedElement = null;
 // defining the main variables that I'll be working with
 const colorBlob = document.querySelector(".blob");
 // individually doing the variables for the colors
@@ -33,37 +32,34 @@ function resetColor(){
 
 function redStartDrag() {
   draggedElement = redBlob;
-  droppedElement = paint;
+  hex2dec("#ff0000");
+
 }
 
 function orangeStartDrag() {
     draggedElement = orangeBlob;
-    droppedElement = paint;
+
   }
   
   function yellowStartDrag() {
     draggedElement = yellowBlob;
-    droppedElement = paint;
+
   }
   function greenStartDrag() {
     draggedElement = greenBlob;
-    droppedElement = paint;
   }
   function blueStartDrag() {
     draggedElement = blueBlob;
-    droppedElement = paint;
+
   }
   function purpleStartDrag() {
     draggedElement = purpleBlob;
-    droppedElement = paint;
   }
   function whiteStartDrag() {
     draggedElement = whiteBlob;
-    droppedElement = paint;
   }
   function blackStartDrag() {
     draggedElement = blackBlob;
-    droppedElement = paint;
   }
 
 
@@ -77,14 +73,73 @@ function endDrag(event) {
 paint.addEventListener("drop", handleDrop);
 
 
+
 function handleDrop() {
   if (draggedElement) {
-    const color = window
-      .getComputedStyle(draggedElement)
-      .getPropertyValue("fill");
-      paint.style.color = color ;
-      paint.style.fill = color ;
+    const color1 = window
+    .getComputedStyle(draggedElement)
+    .getPropertyValue("fill");
+  const color2 = window
+  .getComputedStyle(paint)
+  .getPropertyValue("fill");
+
+    const mixedCol = mix_hexes(color1,color2);
+      paint.style.fill = color1;
     draggedElement = null;
   }
 }
 
+// console.log(mix_hexes('#', '#f6ff00'));
+
+function hex2dec(hex) {
+  return hex.replace('#', '').match(/.{2}/g).map(n => parseInt(n, 16));
+}
+
+function rgb2hex(r, g, b) {
+  r = Math.round(r);
+  g = Math.round(g);
+  b = Math.round(b);
+  r = Math.min(r, 255);
+  g = Math.min(g, 255);
+  b = Math.min(b, 255);
+  return '#' + [r, g, b].map(c => c.toString(16).padStart(2, '0')).join('');
+}
+
+function rgb2cmyk(r, g, b) {
+  let c = 1 - (r / 255);
+  let m = 1 - (g / 255);
+  let y = 1 - (b / 255);
+  let k = Math.min(c, m, y);
+  c = (c - k) / (1 - k);
+  m = (m - k) / (1 - k);
+  y = (y - k) / (1 - k);
+  return [c, m, y, k];
+}
+
+function cmyk2rgb(c, m, y, k) {
+  let r = c * (1 - k) + k;
+  let g = m * (1 - k) + k;
+  let b = y * (1 - k) + k;
+  r = (1 - r) * 255 + .5;
+  g = (1 - g) * 255 + .5;
+  b = (1 - b) * 255 + .5;
+  return [r, g, b];
+}
+
+
+function mix_cmyks(...cmyks) {
+  let c = cmyks.map(cmyk => cmyk[0]).reduce((a, b) => a + b, 0) / cmyks.length;
+  let m = cmyks.map(cmyk => cmyk[1]).reduce((a, b) => a + b, 0) / cmyks.length;
+  let y = cmyks.map(cmyk => cmyk[2]).reduce((a, b) => a + b, 0) / cmyks.length;
+  let k = cmyks.map(cmyk => cmyk[3]).reduce((a, b) => a + b, 0) / cmyks.length;
+  return [c, m, y, k];
+}
+
+function mix_hexes(...hexes) {
+  let rgbs = hexes.map(hex => hex2dec(hex)); 
+  let cmyks = rgbs.map(rgb => rgb2cmyk(...rgb));
+  let mixture_cmyk = mix_cmyks(...cmyks);
+  let mixture_rgb = cmyk2rgb(...mixture_cmyk);
+  let mixture_hex = rgb2hex(...mixture_rgb);
+  return mixture_hex;
+}
