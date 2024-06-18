8ad86c4572030a83b5250961806abda6c2fe2383
ppestoe <130018086+ppestoe@users.noreply.github.com>
2024-06-18 14:28:42 +1000

    j

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,30 +1,70 @@
+// this is so that the other functions can refer to the dragged element
 let draggedElement = null;
-
+// defining the main variables that I'll be working with
 const colorBlob = document.querySelector(".blob");
-const resultBlob = document.querySelector(".result");
-const blobImg = document.querySelector("#blob");
+// individually doing the variables for the colors
+const redBlob = document.querySelector("#red");
+const orangeBlob = document.querySelector("#orange");
+const yellowBlob = document.querySelector("#yellow");
+const greenBlob = document.querySelector("#green");
+const blueBlob = document.querySelector("#blue");
+const purpleBlob = document.querySelector("#purple");
+const whiteBlob = document.querySelector("#white");
+const blackBlob = document.querySelector("#black");
+const resultBlob = document.querySelector("#resultblob");
+const paint = document.querySelector("#paint");
 
-colorBlob.addEventListener("dragstart", startDrag);
+redBlob.addEventListener("dragstart", redStartDrag);  
+orangeBlob.addEventListener("dragstart", orangeStartDrag); 
+yellowBlob.addEventListener("dragstart", yellowStartDrag)
+greenBlob.addEventListener("dragstart", greenStartDrag);
+blueBlob.addEventListener("dragstart", blueStartDrag);
+purpleBlob.addEventListener("dragstart", purpleStartDrag);
+whiteBlob.addEventListener("dragstart", whiteStartDrag);
+blackBlob.addEventListener("dragstart", blackStartDrag);
 
-function startDrag() {
-  draggedElement = colorBlob;
+function redStartDrag() {
+  draggedElement = redBlob;
 }
 
-resultBlob.addEventListener("dragover", endDrag);
+function orangeStartDrag() {
+    draggedElement = orangeBlob;
+  }
+  
+  function yellowStartDrag() {
+    draggedElement = yellowBlob;
+  }
+  function greenStartDrag() {
+    draggedElement = greenBlob;
+  }
+  function blueStartDrag() {
+    draggedElement = blueBlob;
+  }
+  function purpleStartDrag() {
+    draggedElement = purpleBlob;
+  }
+  function whiteStartDrag() {
+    draggedElement = whiteBlob;
+  }
+  function blackStartDrag() {
+    draggedElement = blackBlob;
+  }
+
+paint.addEventListener("dragover", endDrag);
 
 function endDrag(event) {
   event.preventDefault();
 }
 
-resultBlob.addEventListener("drop", handleDrop);
+paint.addEventListener("drop", handleDrop);
 
 function handleDrop() {
   if (draggedElement) {
     const color = window
       .getComputedStyle(draggedElement)
-      .getPropertyValue("background-color");
-      resultBlob.classList.toggle("show");
-    resultBlob.style.backgroundColor = color;
+      .getPropertyValue("fill");
+      paint.style.fill = color ;
+
     draggedElement = null;
   }
 }
