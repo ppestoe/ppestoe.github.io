6f8456fc7f303bdc25e2fa05a814f95a4ceb58ac
ppestoe <130018086+ppestoe@users.noreply.github.com>
2024-06-19 21:11:37 +1000

    s

--- assignment3/script.js
+++ assignment3/script.js
@@ -215,40 +215,41 @@ function rgb2hex(r, g, b) {
   return '#' + [r, g, b].map(c => c.toString(16).padStart(2, '0')).join('');
 }
 
-// these codes were also a part of GirkovArpa's original codes, but I didn't work with CMYK colors in this project so Im commenting them out to not confuse myself
+// these codes were also a part of GirkovArpa's original codes, but I didn't work with CMYK colors in this project so these werent used within my functions
 
-// function hex2dec(hex) {
-//   return hex.replace('#', '').match(/.{2}/g).map(n => parseInt(n, 16));
-// }
 
-// function rgb2cmyk(r, g, b) {
-//   let c = 1 - (r / 255);
-//   let m = 1 - (g / 255);
-//   let y = 1 - (b / 255);
-//   let k = Math.min(c, m, y);
-//   c = (c - k) / (1 - k);
-//   m = (m - k) / (1 - k);
-//   y = (y - k) / (1 - k);
-//   return [c, m, y, k];
-// }
+function hex2dec(hex) {
+  return hex.replace('#', '').match(/.{2}/g).map(n => parseInt(n, 16));
+}
 
-// function cmyk2rgb(c, m, y, k) {
-//   let r = c * (1 - k) + k;
-//   let g = m * (1 - k) + k;
-//   let b = y * (1 - k) + k;
-//   r = (1 - r) * 255 + .5;
-//   g = (1 - g) * 255 + .5;
-//   b = (1 - b) * 255 + .5;
-//   return [r, g, b];
-// }
+function rgb2cmyk(r, g, b) {
+  let c = 1 - (r / 255);
+  let m = 1 - (g / 255);
+  let y = 1 - (b / 255);
+  let k = Math.min(c, m, y);
+  c = (c - k) / (1 - k);
+  m = (m - k) / (1 - k);
+  y = (y - k) / (1 - k);
+  return [c, m, y, k];
+}
 
-// function mix_cmyks(...cmyks) {
-//   let c = cmyks.map(cmyk => cmyk[0]).reduce((a, b) => a + b, 0) / cmyks.length;
-//   let m = cmyks.map(cmyk => cmyk[1]).reduce((a, b) => a + b, 0) / cmyks.length;
-//   let y = cmyks.map(cmyk => cmyk[2]).reduce((a, b) => a + b, 0) / cmyks.length;
-//   let k = cmyks.map(cmyk => cmyk[3]).reduce((a, b) => a + b, 0) / cmyks.length;
-//   return [c, m, y, k];
-// }
+function cmyk2rgb(c, m, y, k) {
+  let r = c * (1 - k) + k;
+  let g = m * (1 - k) + k;
+  let b = y * (1 - k) + k;
+  r = (1 - r) * 255 + .5;
+  g = (1 - g) * 255 + .5;
+  b = (1 - b) * 255 + .5;
+  return [r, g, b];
+}
+
+function mix_cmyks(...cmyks) {
+  let c = cmyks.map(cmyk => cmyk[0]).reduce((a, b) => a + b, 0) / cmyks.length;
+  let m = cmyks.map(cmyk => cmyk[1]).reduce((a, b) => a + b, 0) / cmyks.length;
+  let y = cmyks.map(cmyk => cmyk[2]).reduce((a, b) => a + b, 0) / cmyks.length;
+  let k = cmyks.map(cmyk => cmyk[3]).reduce((a, b) => a + b, 0) / cmyks.length;
+  return [c, m, y, k];
+}
 
 function mix_hexes(...hexes) {
   let rgbs = hexes.map(hex => hex2dec(hex)); 
